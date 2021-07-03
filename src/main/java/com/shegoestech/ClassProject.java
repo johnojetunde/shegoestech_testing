11714b0a307a0b9cbeef0e4666c3e031b177cf7f
johnojetunde <johnojetunde63@gmail.com>
2021-07-03 13:18:12 +0300

    class project

---
+++ src/main/java/com/shegoestech/ClassProject.java
@@ -0,0 +1,24 @@
+package com.shegoestech;
+
+public class ClassProject {
+
+    /**
+     * Create a product stocking system that does the following:
+     * 1. create the products and stock it with a particular quantity
+     * 2. search for product by name; if a product does not exist, throw an exception
+     * 3. search for products by quantity; return list of products that matches that quantity
+     * 4. Print the result of your search any way you want
+     *
+     * Constraints:
+     * use hibernate & JPA
+     * write unit tests for your methods
+     * integration tests are nice-haves
+     * design a small API for the solution above:
+     *      /products -  GET - for getting all products
+     *     /products/{id} -  GET - for getting product by id
+     *
+     *
+     * Tips:
+     * Use update mode for your HDDML_AUTO
+     * */
+}
